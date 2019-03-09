59aaa6c09c23d9e5954fc3f2c33052cf32d5e2b4
Luke Fiorio <lukefiorio@Lukes-MBP.home>
2019-03-08 21:51:42 -1000

    finished

--- scope.js
+++ scope.js
@@ -3,10 +3,12 @@ var isCookieJarOpen = null;
 
 function closeLid() {
   /* answer here */
+  return isCookieJarOpen = false;
 }
 
 function openLid() {
   /* answer here */
+  return isCookieJarOpen = true;
 }
 
 
@@ -17,7 +19,7 @@ function outerFunction() {
 
   function innerFunction() {
     var world = 'World';
-    return /* answer here */;
+    return hello+" "+world;
   }
   return innerFunction();
 }
@@ -31,8 +33,8 @@ function addMatrixElements(matrix) {
 
   for(var i = 0; i < matrix.length; i++) {
     /* fix counter variables in the second loop */
-    for(var i = 0; i < matrix[i].length; i++) {
-      result = result + matrix[i][i];
+    for(var j = 0; j < matrix[i].length; j++) {
+      result = result + matrix[i][j];
     }
   }
   return result;
@@ -49,16 +51,16 @@ function sendDataToClient() {
   }
 
   function authenticateUser(obj, username) {
-    var userObject = {
+    let userObject = {
       handle: 'morpheus',
       authenticated: false
     };
 
-    if (userObject.handle === username) {
-      userObject.authenticated = true;
-      return userObject
+    if (obj.handle === username) {
+      obj.authenticated = true;
+      return obj
     } else {
-      return userObject
+      return obj
     }
   }
   authenticateUser(userObject, 'neo')
